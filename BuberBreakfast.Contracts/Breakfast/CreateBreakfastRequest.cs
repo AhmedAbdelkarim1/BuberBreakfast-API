49c39501d260c17be0a607012fe9cd6f9f24581b
20912019100756 <20912019100756@fci.zu.edu.eg>
2025-09-01 14:34:50 +0300

    Add project files.

---
+++ BuberBreakfast.Contracts/Breakfast/CreateBreakfastRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberBreakfast.Contracts.Breakfast
+{
+	public record CreateBreakfastRequest
+	{
+		string Name;
+		string Description;
+		DateTime StartDateTime;
+		DateTime EndDateTime;
+		List<string> Savory;
+		List<string> Sweet;
+
+		public CreateBreakfastRequest
+			(string name
+			, string description
+			, DateTime startDateTime
+			, DateTime endDateTime
+			, List<string> savory
+			, List<string> sweet)
+		{
+			Name = name;
+			Description = description;
+			StartDateTime = startDateTime;
+			EndDateTime = endDateTime;
+			Savory = savory;
+			Sweet = sweet;
+		}
+
+	}
+}
---
+++ BuberBreakfast/Program.cs
@@ -0,0 +1,35 @@
+using Scalar.AspNetCore;
+
+namespace BuberBreakfast
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            app.MapOpenApi();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapScalarApiReference();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+
+			app.Run();
+        }
+    }
+}
